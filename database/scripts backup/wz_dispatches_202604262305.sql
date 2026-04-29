dfb16dd73854f7de8127b8748607c37eebc623e9
Weldercris Ribeiro <weldercris.ribeiro.dev@gmail.com>
2026-04-29 15:07:14 -0300

    ajuste branch pg docker

---
+++ database/scripts backup/wz_dispatches_202604262305.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.wz_dispatches (title,"content","type",target_group_id,target_user_id,priority,status,scheduled_at,sent_at,error_message,created_by,created_at,id,attachment_url) VALUES
+	 ('Teste','*Cris:*
+aaaaaa','individual',NULL,'48cba748-94ce-4e34-aac1-7d02a9d81ff4','normal','sent','2026-04-26 21:39:00','2026-04-26 21:39:08.628',NULL,'c52bc81a-0473-4731-b01a-ece82d298c43','2026-04-26 21:38:04.177','068cebbb-c201-4bdb-9be6-1baf36e30b34'::uuid,NULL),
+	 ('teste','*Cris:*
+teste enviando pelo back do render ativo.😊','general',NULL,NULL,'normal','sent','2026-04-26 22:10:00','2026-04-26 22:10:15.745',NULL,'c52bc81a-0473-4731-b01a-ece82d298c43','2026-04-26 22:09:06.256','a620e665-fb3a-4e29-a147-5228d736a234'::uuid,NULL),
+	 ('aaaa','*Cris:*
+a','general',NULL,NULL,'urgent','sent','2026-04-26 22:12:00','2026-04-26 22:12:17.452',NULL,'c52bc81a-0473-4731-b01a-ece82d298c43','2026-04-26 22:10:09.181','c7efdec0-f5fc-40b5-8ac2-e2cad16c81f2'::uuid,NULL),
+	 ('Teste','*Cris:*
+Teste😄','individual',NULL,'b9c13010-adcd-4754-84c8-1be1f76230a0','urgent','sent','2026-04-26 22:53:00','2026-04-26 22:54:09.775',NULL,'c52bc81a-0473-4731-b01a-ece82d298c43','2026-04-26 22:53:26.159','8354d027-19db-421a-8826-04c9be774a72'::uuid,NULL);
